e09a770ec3c26cff2f5451ea79a9e2f85345920b
fabianreateguiperez <2007068@newton.pe>
2022-03-28 15:50:50 -0500

    InitialCommit

--- src/main/java/org/example/AnalysisScreenController.java
+++ src/main/java/org/example/AnalysisScreenController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class AnalysisScreenController {
-
+//
     public void switchToMainScreen(ActionEvent actionEvent) throws IOException {
         App.setRoot("MainScreen");
     }
