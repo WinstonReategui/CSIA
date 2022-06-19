4f8ef02e243e4194857198c104f73cb26eed4332
fabianreateguiperez <2007068@newton.pe>
2022-06-19 18:45:38 -0500

    InitialCommit

--- src/main/java/org/example/LoginScreenController.java
+++ src/main/java/org/example/LoginScreenController.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 
 public class LoginScreenController {
 
-
     public void switchToMainScreen(ActionEvent actionEvent) throws IOException {
-        App.setRoot("MainScreen");
-    }
+            App.setRoot("MainScreen");
+        }
+
 }
